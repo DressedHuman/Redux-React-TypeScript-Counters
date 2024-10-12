140fef74a47e36a3d649a192104bd594fe0959fa
Motiur Rahman Mizan <dressed.human@gmail.com>
2024-10-12 13:41:45 +0600

    initialized the project with react, redux and typescript installed

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="min-w-[100vw] min-h-[100vh] flex justify-center items-center">
+      <h2 className="text-2xl font-medium">Counters</h2>
+    </div>
+  )
+}
+
+export default App

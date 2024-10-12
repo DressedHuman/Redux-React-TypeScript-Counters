092703b4d7fe630d4e28f740cb9b8d3961815d65
Motiur Rahman Mizan <dressed.human@gmail.com>
2024-10-12 16:45:06 +0600

    displayed the functional total counts card

--- src/App.tsx
+++ src/App.tsx
@@ -21,13 +21,14 @@ const initialCounts: InitialCounts = [
 
 function App() {
   const [counters, setCounters] = useState(initialCounts);
+  const totalCounts = counters.reduce((sum, current) => sum + current.value, 0);
 
   const increment = (counterId: number) => {
     const updatedCounters = counters.map(counter => {
       if (counter.id === counterId) {
         return {
           ...counter,
-          value: counter.value+1,
+          value: counter.value + 1,
         };
       }
       return counter;
@@ -39,10 +40,10 @@ function App() {
 
   const decrement = (counterId: number) => {
     const updatedCounters = counters.map(counter => {
-      if(counter.id === counterId) {
+      if (counter.id === counterId) {
         return {
           ...counter,
-          value: counter.value-1,
+          value: counter.value - 1,
         }
       }
       return counter;
@@ -54,15 +55,23 @@ function App() {
   return (
     <div className="min-w-[100vw] min-h-[100vh] flex flex-col justify-center items-center space-y-10">
       <h2 className="text-2xl font-medium">Simple Counter Application</h2>
-      <div className="flex justify-center items-center gap-7">
-        {
-          counters.map(counter => <Counter
-            key={counter.id}
-            count={counter.value}
-            incrementHandler={() => increment(counter.id)}
-            decrementHandler={() => decrement(counter.id)}
-          />)
-        }
+      <div className="space-y-5">
+        {/* counter cards */}
+        <div className="flex justify-center items-center gap-7">
+          {
+            counters.map(counter => <Counter
+              key={counter.id}
+              count={counter.value}
+              incrementHandler={() => increment(counter.id)}
+              decrementHandler={() => decrement(counter.id)}
+            />)
+          }
+        </div>
+
+        {/* total counts card */}
+        <div className="w-full px-5 py-3 flex justify-center items-center bg-white">
+          <p className="text-black text-xl font-semibold">Total Count(s): {totalCounts}</p>
+        </div>
       </div>
     </div>
   )
